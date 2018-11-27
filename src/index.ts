84de861c5772ee515b98ec596db7287d314910be
David Osborne <david@digital-clouds.com>
2018-11-27 23:10:53 +0000

    Added some console logging, and fixed a mistyped function call

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import {Socket} from "socket.io";
 
-const server = require("http").createClient();
+const server = require("http").createServer();
 const io = require("socket.io")(server);
 const port  = process.env.PORT || 3000;
 
-
+console.log(`listening on ${port}`);
 io.on("connection", (socket: Socket) => {
     // called whenever a new client connects
+    console.log("client connected");
 });
 
 server.listen(port);
